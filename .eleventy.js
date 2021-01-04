ec9295452772bb641c66376ca6836f28826d4f33
Peter deHaan <peter@deseloper.com>
2021-01-04 08:13:43 -0800

    Add Nunjucks example

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
+const md = require("markdown-it")({
+  html: true
+});
+
 module.exports = eleventyConfig => {
+  eleventyConfig.addFilter("markdown", content => md.render(content));
+
   return {
     dir: {
       input: "src",
